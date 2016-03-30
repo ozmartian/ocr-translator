6adab34ec0967d144454b30bc48168ddbb2ed913
Pete Alexandrou <pete@ozmartians.com>
2016-03-30 13:19:57 +0800

    first draft complete w/ amendments to darkroomjs for img upload

--- js/main.js
+++ js/main.js
@@ -16,14 +16,21 @@ var OCR = {
         }
         return new Blob([ia], { type: mimeString });
     },
+    adjustTextarea: function(el, minHeight) {
+        if (!minHeight) { minHeight = 24; }
+        var outerHeight = parseInt(window.getComputedStyle(el).height, 10);
+        var diff = outerHeight - el.clientHeight;
+        el.style.height = 0;
+        el.style.height = Math.max(minHeight, el.scrollHeight + diff) + 'px';
+    },
     recognize: function(image) {
         $('#result').text('');
         $('.hide:visible').fadeOut(2000);
         var data = new FormData();
         data.append("apikey", this.apiKey);
         data.append("language", "chs");
-        data.append("isOverlayRequired", true),
-            data.append("file", this.dataURI2Blob(image), "image.png");
+        data.append("isOverlayRequired", true);
+        data.append("file", this.dataURI2Blob(image), "image.png");
         $.ajax({
             url: this.apiUrl,
             type: 'POST',
@@ -39,7 +46,8 @@ var OCR = {
                 var ocrText = res.ParsedResults[0].ParsedText.trim();
                 $('#result').text(ocrText);
                 $('.hide:hidden').fadeIn(2000, function() {
-                    adjustTextarea(document.getElementById('result'), 24);
+                    OCR.adjustTextarea(document.getElementById('result'));
+                    imgEditor.progress('translating text...');
                     translate.init(ocrText);
                 }).css('display', 'inline-block');
             } else {
@@ -50,20 +58,15 @@ var OCR = {
     }
 };
 
-var adjustTextarea = function(el, minHeight) {
-    var outerHeight = parseInt(window.getComputedStyle(el).height, 10);
-    var diff = outerHeight - el.clientHeight;
-    el.style.height = 0;
-    el.style.height = Math.max(minHeight, el.scrollHeight + diff) + 'px';
-};
-
 var translate = {
     youdaoUrl: "https://fanyi.youdao.com/openapi.do?keyfrom=friskfly&key=1410212834&type=data&doctype=jsonp&version=1.1&callback=translate.youdao_callback&q=",
     yandexUrl: "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20160303T163724Z.5f03d81e233214a0.2a3088b8385dd6e0adffec8d224d405f5a1fe171&lang=zh-en&callback=translate.yandex_callback&text=",
-    youdao: { jsonp: null },
-    yandex: { jsonp: null },
+    youdao: { jsonp: null, done: false },
+    yandex: { jsonp: null, done: false },
     translation: null,
     youdao_callback: function(response) {
+        translate.youdao.done = true;
+        if (translate.youdao.done && translate.yandex.done) { imgEditor.progress(); }
         if (typeof (response.translation) != "undefined" && response.translation.length > 0) {
             $('.youdao').text();
             $.each(response.translation, function(i, val) {
@@ -79,6 +82,7 @@ var translate = {
         return false;
     },
     initYoudao: function(txt) {
+        translate.youdao.done = false;
         if (!document.getElementById('youdao_script') || typeof translate.youdao.jsonp === "undefined") {
             translate.youdao.jsonp = document.createElement("script");
             translate.youdao.jsonp.id = "youdao_script";
@@ -87,6 +91,8 @@ var translate = {
         translate.youdao.jsonp.src = this.youdaoUrl + encodeURIComponent(txt);
     },
     yandex_callback: function(response) {
+        translate.yandex.done = true;
+        if (translate.youdao.done && translate.yandex.done) { imgEditor.progress(); }
         if (response.code == 200 && response.text.length > 0) {
             $('.yandex').text(response.text[0].trim());
             $('.yandex').fadeIn(2000);
@@ -98,6 +104,7 @@ var translate = {
         return false;
     },
     initYandex: function(txt) {
+        translate.yandex.done = false;
         if (!document.getElementById('yandex_script') || typeof translate.yandex.jsonp === "undefined") {
             translate.yandex.jsonp = document.createElement("script");
             translate.yandex.jsonp.id = "yandex_script";
@@ -111,57 +118,38 @@ var translate = {
     }
 };
 
-var dkrm = new Darkroom('#target', {
-    minWidth: 100,
-    minHeight: 100,
-    maxWidth: 600,
-    maxHeight: 500,
-    ratio: 4 / 3,
-    backgroundColor: '#FFF',
-    plugins: {
-        crop: { quickCropKey: 67 },
-        save: {
-            callback: function() {
-                this.darkroom.selfDestroy();
-                var newImage = dkrm.canvas.toDataURL();
-                OCR.recognize(newImage);
-            }
+var dkrm;
+var imgEditor = {
+    progress: function(txt) {
+        var el = $('.progress:hidden');
+        if (txt || el.length) {
+            $('.progress > .label').text(txt);
+            $('.progress:hidden').show();
+        } else {
+            $('.progress').fadeOut('slow');
         }
     },
-    initialize: function() {
-        var cropPlugin = this.plugins['crop'];
-        // cropPlugin.selectZone(170, 25, 300, 300);
-        cropPlugin.requireFocus();
-    }
-});
-
-/*
-var imageutil = {
-    render: function(src) {
-        var image = new Image();
-        image.onload = function() {
-            var ctx = dkrm.canvas.getContext("2d");
-            ctx.clearRect(0, 0, dkrm.canvas.width, dkrm.canvas.height);
-            canvas.width = image.width;
-            canvas.height = image.height;
-            ctx.drawImage(image, 0, 0, image.width, image.height);
-        };
-        image.src = src;
-    },
-    loadImage: function(src) {
-        var reader = new FileReader();
-        reader.onload = function(e) { this.render(e.target.result); }
-        reader.readAsDataURL(src);
-    },
-    init: function() {
-        var target = $('.drop-target');
-        target.on('dragover', function(e) { e.preventDefault(); });
-        target.on('drop', function(e) {
-            e.preventDefault();
-            e.dataTransfer = e.originalEvent && e.originalEvent.dataTransfer;
-            console.log(e.dataTransfer.getData());
-            this.loadImage(e.dataTransfer.files[0]);
+    init: function(el) {
+        dkrm = new Darkroom(el, {
+            minWidth: 100,
+            minHeight: 100,
+            maxWidth: 800,
+            //maxHeight: 500,
+            ratio: 4 / 3,
+            backgroundColor: '#FFF',
+            plugins: {
+                save: {
+                    callback: function() {
+                        imgEditor.progress('converting image to text...');
+                        this.darkroom.selfDestroy();
+                        var newImage = dkrm.canvas.toDataURL();
+                        OCR.recognize(newImage);
+                    }
+                }
+            },
+            initialize: function() { }
         });
     }
 };
-*/
+
+imgEditor.init('#target');
