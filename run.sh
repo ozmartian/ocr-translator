10aad36b25143485e66e4ba8e4ec480af866dfef
Pete Alexandrou <pete@ozmartians.com>
2016-05-12 12:16:14 +0800

    Screenshot python tool workinggit add -A .

---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+SCREENSHOT_TOOLPATH=`which spectacle`
+SCREENSHOT_SAVEPATH="/tmp/ocr-translator-`date +%Y%m%d-%H%M%S`.png"
--- screenshot.py
+++ screenshot.py
@@ -1,16 +1,16 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
-import wx, time
+import wx, time, sys
 
 #--------------------------------------------------------------------------------------------------------#
 
-class SelectableFrame(wx.Frame):
+class MainFrame(wx.Frame):
     c1 = None
     c2 = None
 
     def __init__(self, parent=None, id=-1, title=""):
         wx.Frame.__init__(self, parent, id, title, pos=(0, 0), size=wx.DisplaySize(), style=wx.FRAME_NO_TASKBAR | wx.NO_BORDER | wx.STAY_ON_TOP)
-
+        
         self.panel = wx.Panel(self, size=self.GetSize())
 
         self.panel.Bind(wx.EVT_MOTION, self.OnMouseMove)
@@ -22,13 +22,13 @@ class SelectableFrame(wx.Frame):
         
         self.Bind(wx.EVT_CLOSE, self.OnClose)
         
-        self.SetCursor(wx.StockCursor(wx.CURSOR_CROSS))
+        self.SetCursor(wx.Cursor(wx.CURSOR_CROSS))
 
         self.panel.SetBackgroundColour(wx.Colour(0, 0, 0))
-        self.panel.SetBackgroundStyle(wx.BG_STYLE_COLOUR)
-        
+        #self.panel.SetBackgroundStyle(wx.BG_STYLE_COLOUR)
+
         self.SetTransparent(150)
-        
+             
     def OnClose(self, event):
         self.Destroy()
         
@@ -38,31 +38,32 @@ class SelectableFrame(wx.Frame):
             self.Refresh()
 
     def OnMouseSelect(self, event):
-        self.SetCursor(wx.StockCursor(wx.CURSOR_CROSS))
+        self.SetCursor(wx.Cursor(wx.CURSOR_CROSS))
         self.c1 = event.GetPosition()
 
     def OnMouseUp(self, event):
-        self.SetCursor(wx.StockCursor(wx.CURSOR_ARROW))
+        self.SetCursor(wx.Cursor(wx.CURSOR_ARROW))
         
     def OnKeyDown(self, event):
         key = event.GetKeyCode()
         if key == wx.WXK_ESCAPE:
             self.Close()
         elif (key == wx.WXK_RETURN or key == wx.WXK_NUMPAD_ENTER) and self.RegionSelected():
-            self.TakeScreenshot()
+            data = ScreenshotData(self.c1.x, self.c1.y, self.c2.x - self.c1.x, self.c2.y - self.c1.y)
+            self.TakeScreenshot(data)
+            self.Close()
         event.Skip()
         
-    def OnReset(self, event=None):
+    def OnReset(self, event):
         wx.PaintDC(self.panel).Clear()
-        self.SetCursor(wx.StockCursor(wx.CURSOR_CROSS))
+        self.SetCursor(wx.Cursor(wx.CURSOR_CROSS))
 
     def OnPaint(self, event):
         if not self.RegionSelected():
             return
         dc = wx.PaintDC(self.panel)
-        dc.SetPen(wx.Pen("white", 1, wx.LONG_DASH))
+        dc.SetPen(wx.Pen(wx.WHITE, 1, wx.LONG_DASH))
         dc.SetBrush(wx.Brush(wx.Colour(100, 100, 100), wx.SOLID))
-
         dc.DrawRectangle(self.c1.x, self.c1.y, self.c2.x - self.c1.x, self.c2.y - self.c1.y)
 
     def RegionSelected(self):
@@ -71,33 +72,54 @@ class SelectableFrame(wx.Frame):
         else:
             return True
 
-    def TakeScreenshot(self):
-        if not self.RegionSelected():
-            return
-        wx.PaintDC(self.panel).Clear()
-        self.Hide()
-        self.Refresh()
-        dcScreen = wx.ScreenDC()
-        bmp = wx.EmptyBitmap(self.c2.x - self.c1.x, self.c2.y - self.c1.y)
-        memDC = wx.MemoryDC()
-        memDC.SelectObject(bmp)
-        memDC.Blit(0, 0, self.c2.x - self.c1.x, self.c2.y - self.c1.y, dcScreen, self.c1.x, self.c1.y)
-        memDC.SelectObject(wx.NullBitmap)
-        img = bmp.ConvertToImage()
-        filename = "/tmp/ocr-screenshot-" + time.strftime('%Y%m%d-%H%M%S') + ".png"
-        img.SaveFile(filename, wx.BITMAP_TYPE_PNG)
-        self.Close()
+    def TakeScreenshot(self, data):
+        if isinstance(data, ScreenshotData):
+            #dcScreen = wx.ScreenDC()
+            #bmp = wx.Bitmap(data.width, data.height)
+            #memDC = wx.MemoryDC()
+            #memDC.SelectObject(bmp)
+            #memDC.Blit(0, 0, data.width, data.height, dcScreen, data.x, data.y)
+            #memDC.SelectObject(wx.NullBitmap)
+            global app
+            bmp = app.GetDesktop().GetSubBitmap(wx.Rect(data.x, data.y, data.width, data.height))
+            img = bmp.ConvertToImage()
+            filename = "/tmp/ocr-screenshot-" + time.strftime('%Y%m%d-%H%M%S') + ".png"
+            img.SaveFile(filename, wx.BITMAP_TYPE_PNG)
+        
+#--------------------------------------------------------------------------------------------------------#
 
-#--------------------------------------------------------------------------------------------------------
-
-class SelectableApp(wx.App):
+class Screenshoter(wx.App):
+    bmp = None
+    
     def OnInit(self):
-        self.frame = SelectableFrame()
+        self.SaveDesktop()
+        self.frame = MainFrame(None)
         self.frame.Show(True)
         self.SetTopWindow(self.frame)
         return True
+        
+    def SaveDesktop(self):
+        dcScreen = wx.ScreenDC()
+        self.bmp = wx.Bitmap(dcScreen.Size.Width, dcScreen.Size.Height)
+        memDC = wx.MemoryDC()
+        memDC.SelectObject(self.bmp)
+        memDC.Blit(0, 0, dcScreen.Size.Width, dcScreen.Size.Height, dcScreen, 0, 0)
+        memDC.SelectObject(wx.NullBitmap)
+        
+    def GetDesktop(self):
+        if self.bmp.IsOk():
+            return self.bmp
 
 #--------------------------------------------------------------------------------------------------------#
 
-app = SelectableApp(False)
+class ScreenshotData:
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
+#--------------------------------------------------------------------------------------------------------#
+
+app = Screenshoter(False)
 app.MainLoop()
